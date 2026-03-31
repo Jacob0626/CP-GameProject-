e90c1cf17fb783eaffbb8cd42bc0014798f9971a
Jacob0626 <jgonzalez8@caldwell.edu>
2026-03-31 02:16:33 +0000

    Mar 30, 2026, 10:16 PM

--- src/main.py
+++ src/main.py
@@ -4,4 +4,10 @@ import pygame
 pygame.init()
 width = 1000
 height = 600
-screen = pygame.display.setmode((width, height))
+screen = pygame.display.set_mode((width, height))
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.quit:
+            run = False
