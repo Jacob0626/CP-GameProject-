4470482c62a28134ed05c6f2e149448979f5cbaa
Jacob0626 <jgonzalez8@caldwell.edu>
2026-03-31 02:27:16 +0000

    Mar 30, 2026, 10:27 PM

--- src/main.py
+++ src/main.py
@@ -6,12 +6,12 @@ width = 1000
 height = 600
 screen = pygame.display.set_mode((width, height))
 
+player = pygame.Rect((200, 350, 50, 50))
+
+
 run = True
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
 pygame.quit()
-
-
