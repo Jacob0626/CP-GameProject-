06024cf02049664edce35aba52fedb09a34fe67d
Jacob0626 <jgonzalez8@caldwell.edu>
2026-04-14 01:05:47 +0000

    Apr 13, 2026, 9:05 PM

--- src/main.py
+++ src/main.py
@@ -27,6 +27,7 @@ facing_right = True
 boss = pygame.Rect((800, 130, 50, 70))
 boss_max_hp = 10
 boss_hp = 10
+boss_bar_bg = pygame.Rect(700, 30, 200, 20)
 
 # ---------- Sandwich ----------
 sandwich = pygame.Rect((940, 160, 30, 40))
