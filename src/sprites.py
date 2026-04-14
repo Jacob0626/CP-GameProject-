4f8efae1b535b6458ef80dd34ba3e562fe54a5ed
Jacob0626 <jgonzalez8@caldwell.edu>
2026-04-14 22:58:48 +0000

    Apr 14, 2026, 6:58 PM

--- src/sprites.py
+++ src/sprites.py
@@ -10,3 +10,20 @@ class Player:
         self.facing_right = True
         self.hits = 0
         self.can_shoot = False
+    
+    def reset(self):
+        self.x = 40
+        self.y = 460
+        self.rect = pygame.Rect(int(self.x), 450, 50, 50)
+        self.velocity_y = 0
+        self.on_ground = True
+        self.facing_right = True
+        self.hits = 0
+        self.can_shoot = False
+
+
+class Boss:
+    def __init__(self):
+        self.rect = pygame.Rect(800, 130, 50, 70)
+        self.max_hp = 10
+        
