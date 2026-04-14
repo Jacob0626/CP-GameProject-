f64f17505d067a32ee9ff30b951234576639648e
Jacob0626 <jgonzalez8@caldwell.edu>
2026-04-14 03:51:51 +0000

    Apr 13, 2026, 11:51 PM

--- src/main.py
+++ src/main.py
@@ -87,8 +87,8 @@ while run:
             boss.left = boss_left_limit
             boss_direction = 1
         
-        if boss.right >= boss_left_limit:
-            boss.right = boss_left_limit
+        if boss.right >= boss_right_limit:
+            boss.right = boss_right_limit
             boss_direction = -1
         
         if key[pygame.K_a]:
