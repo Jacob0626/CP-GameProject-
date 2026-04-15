9687cef4e24afcaf2ce26a5cdfa1a504532556c6
Jacob0626 <jgonzalez8@caldwell.edu>
2026-04-15 04:40:30 +0000

    Apr 15, 2026, 12:40 AM

--- src/main.py
+++ src/main.py
@@ -22,7 +22,7 @@ boss_image = pygame.transform.scale(boss_image, (50, 70))
 sandwich_image = pygame.image.load("src/assets/Sandwich.png").convert_alpha()
 sandwich_image = pygame.transform.scale(sandwich_image, (30, 40))
 
-platform_oneway_image = pygame.image.load("src")
+platform_oneway_image = pygame.image.load("src/assets/")
 
 # ---------- Constants ----------
 gravity = 0.6 
